1dbadf6468d6f6357a4083f21e1c98e1670249f3
Naesa Orlina <naesa87@gmail.com>
2015-04-16 20:06:01 +1000

    Create MainMenu Test

---
+++ test/com/twu/biblioteca/models/MainMenuTest.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MainMenuTest {
+
+    private List<String> expectedOptions = new ArrayList<String>();
+    private MainMenu mainMenu = new MainMenu();
+
+    @Before
+    public void setUp() throws Exception {
+        expectedOptions.add("List Books");
+    }
+
+    @Test
+    public void testGetOptions(){;
+        assertEquals("Incorrect options", expectedOptions, mainMenu.getOptions());
+    }
+
+    @Test
+    public void testToString(){
+        String expectedString =
+                "============================================================================================================\n" +
+                "MAIN MENU OPTIONS\n" +
+                "List Books\n" +
+                "============================================================================================================";
+        assertEquals("Incorrect String printout",expectedString, mainMenu.toString());
+    }
+}
